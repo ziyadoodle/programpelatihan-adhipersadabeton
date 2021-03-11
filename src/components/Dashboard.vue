a32ee88ee9bfc8cd4da0b936d1bf85031bd21c60
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 11:59:48 +0700

    Create Dashboard.vue

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>asd</p>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.h-screen {
+    background: url("./../assets/double-bubble-outline.png");
+}
+</style>
