5dbf7a18dcd97d245341a1989a3b85c3edae612a
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 12:00:06 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <app-dashboard></app-dashboard>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Dashboard from "@/components/Dashboard.vue"
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+    components: {
+        "app-dashboard": Dashboard,
+    },
+}
 </script>
+
+<style scoped>
+::-webkit-scrollbar {
+    width: 5px;
+    height: 6px;
+}
+
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px #a5aaad;
+    border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+    background: #e02424;
+    border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: #666;
+}
+</style>
