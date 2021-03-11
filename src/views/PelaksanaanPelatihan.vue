94cc7461390b4c93e56cdb79367d52f98e4a0ae1
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 15:59:33 +0700

    Create PelaksanaanPelatihan.vue

---
+++ src/views/PelaksanaanPelatihan.vue
@@ -0,0 +1,13 @@
+<template>
+    <app-pelaksanaan></app-pelaksanaan>
+</template>
+
+<script>
+import PelaksanaanPelatihan from "@/components/pelaksanaanPelatihan.vue"
+
+export default {
+    components: {
+        "app-pelaksanaan": PelaksanaanPelatihan,
+    },
+}
+</script>
