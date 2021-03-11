fe68adce7f95c09760dd4a44088072a348e1a481
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 15:35:07 +0700

    Create Training.vue

---
+++ src/views/Training.vue
@@ -0,0 +1,15 @@
+<template>
+    <app-training></app-training>
+</template>
+
+<script>
+import Training from "@/components/training.vue"
+
+export default {
+    components: {
+        "app-training": Training,
+    },
+}
+</script>
+
+<style scoped></style>
