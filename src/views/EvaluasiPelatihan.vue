a6009ed444b563b1d35ac2478e5ceb75dc8401db
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 15:59:14 +0700

    Create EvaluasiPelatihan.vue

---
+++ src/views/EvaluasiPelatihan.vue
@@ -0,0 +1,13 @@
+<template>
+    <app-evaluasi></app-evaluasi>
+</template>
+
+<script>
+import EvaluasiPelatihan from "@/components/evaluasiPelatihan.vue"
+
+export default {
+    components: {
+        "app-evaluasi": EvaluasiPelatihan,
+    },
+}
+</script>
