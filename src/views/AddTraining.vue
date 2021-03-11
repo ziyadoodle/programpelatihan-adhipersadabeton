fb705b3a8b53158069f8d69fb9f06e79679b015f
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 15:34:57 +0700

    Create AddTraining.vue

---
+++ src/views/AddTraining.vue
@@ -0,0 +1,13 @@
+<template>
+    <app-add-training></app-add-training>
+</template>
+
+<script>
+import AddTraining from "@/components/addTraining.vue"
+
+export default {
+    components: {
+        "app-add-training": AddTraining,
+    },
+}
+</script>
