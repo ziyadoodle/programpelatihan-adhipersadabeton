be38443486bec244efe680042bbb4c3bc7b0b71c
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 15:57:57 +0700

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -409,13 +409,3 @@
         </div>
     </main>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style scoped>
-.h-screen {
-    background: url("./../assets/double-bubble-outline.png");
-}
-</style>
