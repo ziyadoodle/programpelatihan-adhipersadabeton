ac37d73eea9cde991a6113590734114fdca83011
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-11 15:35:14 +0700

    Create Vendor.vue

---
+++ src/views/Vendor.vue
@@ -0,0 +1,13 @@
+<template>
+    <app-vendor></app-vendor>
+</template>
+
+<script>
+import Vendor from "@/components/vendor.vue"
+
+export default {
+    components: {
+        "app-vendor": Vendor,
+    },
+}
+</script>
