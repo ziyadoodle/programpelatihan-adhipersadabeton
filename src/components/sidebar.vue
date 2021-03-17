5c0776c69a0969fe1665a8f184f99979ccf3bf49
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-17 17:47:53 +0700

    Create sidebar.vue

---
+++ src/components/sidebar.vue
@@ -0,0 +1,204 @@
+<template>
+    <aside
+        class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0 animate__animated animate__fadeInLeft animate__slow"
+    >
+        <div class=" py-4 text-gray-500 dark:text-gray-400">
+            <div class="flex justify-center ">
+                <img
+                    src="./../assets/logo-adhi-gray.png"
+                    alt=""
+                    class="w-40 mt-2"
+                />
+            </div>
+            <ul class="mt-6">
+                <li class="relative px-6 py-3">
+                    <router-link
+                        to="/"
+                        class="inline-flex items-center w-full text-sm font-semibold hover:text-gray-800 "
+                        exact
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <rect
+                                x="2"
+                                y="3"
+                                width="20"
+                                height="14"
+                                rx="2"
+                                ry="2"
+                            ></rect>
+                            <line x1="8" y1="21" x2="16" y2="21"></line>
+                            <line x1="12" y1="17" x2="12" y2="21"></line>
+                        </svg>
+                        <span class="ml-4">Dashboard</span>
+                    </router-link>
+                </li>
+            </ul>
+            <ul>
+                <li class="relative px-6 py-3">
+                    <router-link
+                        to="/vendor"
+                        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <rect
+                                x="2"
+                                y="3"
+                                width="20"
+                                height="14"
+                                rx="2"
+                                ry="2"
+                            ></rect>
+                            <line x1="8" y1="21" x2="16" y2="21"></line>
+                            <line x1="12" y1="17" x2="12" y2="21"></line>
+                        </svg>
+                        <span class="ml-4">Vendor</span>
+                    </router-link>
+                </li>
+                <li class="relative px-6 py-3">
+                    <router-link
+                        to="/training"
+                        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <rect
+                                x="2"
+                                y="3"
+                                width="20"
+                                height="14"
+                                rx="2"
+                                ry="2"
+                            ></rect>
+                            <line x1="8" y1="21" x2="16" y2="21"></line>
+                            <line x1="12" y1="17" x2="12" y2="21"></line>
+                        </svg>
+                        <span class="ml-4">Training</span>
+                    </router-link>
+                </li>
+                <li class="relative px-6 py-3">
+                    <router-link
+                        to="/evaluasi-pelatihan"
+                        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <rect
+                                x="2"
+                                y="3"
+                                width="20"
+                                height="14"
+                                rx="2"
+                                ry="2"
+                            ></rect>
+                            <line x1="8" y1="21" x2="16" y2="21"></line>
+                            <line x1="12" y1="17" x2="12" y2="21"></line>
+                        </svg>
+                        <span class="ml-4">Evaluasi Pelatihan</span>
+                    </router-link>
+                </li>
+                <li class="relative px-6 py-3">
+                    <router-link
+                        to="/pelaksanaan-pelatihan"
+                        class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <rect
+                                x="2"
+                                y="3"
+                                width="20"
+                                height="14"
+                                rx="2"
+                                ry="2"
+                            ></rect>
+                            <line x1="8" y1="21" x2="16" y2="21"></line>
+                            <line x1="12" y1="17" x2="12" y2="21"></line>
+                        </svg>
+                        <span class="ml-4">Pelaksanaan Pelatihan</span>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </aside>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isShowing: true,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.router-link-active {
+    @apply text-gray-800;
+    @apply bg-red-600;
+    @apply text-white;
+    @apply p-2;
+    @apply rounded-lg;
+    @apply transition;
+    @apply duration-500;
+}
+
+/* v-bind:class="[isActive ? activeClass : '', errorClass]" */
+
+/* .router-link-active span {
+    @apply absolute;
+    @apply inset-y-0;
+    @apply left-0;
+    @apply w-1;
+    @apply bg-red-600;
+    @apply rounded-tr-lg;
+    @apply rounded-br-lg;
+} */
+</style>
