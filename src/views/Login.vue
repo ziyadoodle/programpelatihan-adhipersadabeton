4cfc3d303983a36f23901f17d23b0fda2ebf2b41
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-20 15:32:58 +0700

    Create Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <app-login></app-login>
+</template>
+
+<script>
+import login from "@/components/login.vue"
+
+export default {
+    components: {
+        "app-login": login,
+    },
+}
+</script>
