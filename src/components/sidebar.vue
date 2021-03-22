1ad4aedc764194f4f86d8b792b66b44619121bba
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-22 19:39:02 +0700

    Update sidebar.vue

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,8 +1,8 @@
 <template>
     <aside
-        class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0 animate__animated animate__fadeInLeft animate__slow"
+        class="z-20 hidden w-64 overflow-y-auto bg-white md:block flex-shrink-0 animate__animated animate__fadeInLeft animate__slow"
     >
-        <div class=" py-4 text-gray-500 dark:text-gray-400">
+        <div class=" py-4 text-gray-500">
             <div class="flex justify-center ">
                 <img
                     src="./../assets/logo-adhi-gray.png"
