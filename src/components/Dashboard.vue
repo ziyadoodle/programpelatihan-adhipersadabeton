5ae9223742080a4553af34175f8921b9e5610c4b
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-22 19:37:25 +0700

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,24 +3,22 @@
         <div
             class="container px-6 mx-auto grid animate__animated animate__fadeInUpBig animate__slow"
         >
-            <h2
-                class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 uppercase"
-            >
+            <h2 class="my-6 text-2xl font-semibold text-gray-700 uppercase">
                 DASHBOARD SUMMARY PELAKSANAAN PELATIHAN | SDM
             </h2>
 
             <div class="flex flex-wrap gap-3 mb-5">
                 <input
                     type="date"
-                    class="flex items-center justify-between w-28 md:w-44 px-4 py-3 bg-white shadow-md dark:bg-white text-sm font-medium leading-5 transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-red"
+                    class="flex items-center justify-between w-28 md:w-44 px-4 py-3 bg-white shadow-md text-sm font-medium leading-5 transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-red"
                 />
                 <input
                     type="date"
-                    class="flex items-center justify-between w-28 md:w-44 px-4 py-3 bg-white shadow-md dark:bg-white text-sm font-medium leading-5 transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-red"
+                    class="flex items-center justify-between w-28 md:w-44 px-4 py-3 bg-white shadow-md text-sm font-medium leading-5 transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-red"
                 />
                 <div class="flex items-center justify-between">
                     <button
-                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red dark:bg-red-600"
+                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red"
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -43,68 +41,52 @@
             <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
                 <!-- Card -->
                 <div
-                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-orange-400 dark:bg-gray-800 transform transition duration-500 hover:scale-110"
+                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-orange-400 transform transition duration-500 hover:scale-110"
                 >
                     <div>
-                        <p
-                            class="mb-2 text-sm font-medium text-orange-500 dark:text-orange-400"
-                        >
+                        <p class="mb-2 text-sm font-medium text-orange-500">
                             Pelatihan Soft Skill
                         </p>
-                        <p
-                            class="text-lg font-semibold text-gray-700 dark:text-gray-200"
-                        >
+                        <p class="text-lg font-semibold text-gray-700">
                             0 Pelatihan
                         </p>
                     </div>
                 </div>
                 <!-- Card -->
                 <div
-                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-blue-400 dark:bg-gray-800 transform transition duration-500 hover:scale-110"
+                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-blue-400 transform transition duration-500 hover:scale-110"
                 >
                     <div>
-                        <p
-                            class="mb-2 text-sm font-medium text-blue-500 dark:text-blue-400"
-                        >
+                        <p class="mb-2 text-sm font-medium text-blue-500">
                             Pelatihan Teknik
                         </p>
-                        <p
-                            class="text-lg font-semibold text-gray-700 dark:text-gray-200"
-                        >
+                        <p class="text-lg font-semibold text-gray-700">
                             0 Pelatihan
                         </p>
                     </div>
                 </div>
                 <!-- Card -->
                 <div
-                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-green-400 dark:bg-gray-800 transform transition duration-500 hover:scale-110"
+                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-green-400  transform transition duration-500 hover:scale-110"
                 >
                     <div>
-                        <p
-                            class="mb-2 text-sm font-medium text-green-500 dark:text-green-400"
-                        >
+                        <p class="mb-2 text-sm font-medium text-green-500">
                             Pelatihan Non Teknik
                         </p>
-                        <p
-                            class="text-lg font-semibold text-gray-700 dark:text-gray-200"
-                        >
+                        <p class="text-lg font-semibold text-gray-700">
                             0 Pelatihan
                         </p>
                     </div>
                 </div>
                 <!-- Card -->
                 <div
-                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-purple-500 dark:bg-gray-800 transform transition duration-500 hover:scale-110"
+                    class="flex items-center p-4 bg-white rounded-lg shadow-xs border-solid border-2 border-purple-500 transform transition duration-500 hover:scale-110"
                 >
                     <div>
-                        <p
-                            class="mb-2 text-sm font-medium text-purple-600 dark:text-purple-400"
-                        >
+                        <p class="mb-2 text-sm font-medium text-purple-600">
                             Pelatihan General Skill
                         </p>
-                        <p
-                            class="text-lg font-semibold text-gray-700 dark:text-gray-200"
-                        >
+                        <p class="text-lg font-semibold text-gray-700">
                             0 Pelatihan
                         </p>
                     </div>
@@ -114,52 +96,40 @@
             <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-6">
                 <!-- Card -->
                 <div
-                    class="flex items-center p-4 rounded-lg shadow-xs dark:bg-green-700 bg-green-400 col-span-2"
+                    class="flex items-center p-4 rounded-lg shadow-xs bg-green-400 col-span-2"
                 >
                     <div>
-                        <p
-                            class="mb-2 text-sm font-medium text-white dark:text-white"
-                        >
+                        <p class="mb-2 text-sm font-medium text-white">
                             ANGGARAN PELATIHAN YANG DIRENCANAKAN
                         </p>
 
-                        <p
-                            class="text-xl font-semibold text-green-800 dark:text-white"
-                        >
+                        <p class="text-xl font-semibold text-green-800">
                             Rp. 000,000,000
                         </p>
                     </div>
                 </div>
                 <div
-                    class="flex items-center p-4 rounded-lg shadow-xs dark:bg-indigo-700 bg-indigo-500 col-span-2"
+                    class="flex items-center p-4 rounded-lg shadow-xs bg-indigo-500 col-span-2"
                 >
                     <div>
-                        <p
-                            class="mb-2 text-sm font-medium text-white dark:text-white"
-                        >
+                        <p class="mb-2 text-sm font-medium text-white">
                             TOTAL ANGGARAN PELATIHAN YANG TEREALISASI
                         </p>
 
-                        <p
-                            class="text-xl font-semibold text-indigo-900 dark:text-white"
-                        >
+                        <p class="text-xl font-semibold text-indigo-900">
                             Rp. 000,000,000
                         </p>
                     </div>
                 </div>
                 <div
-                    class="flex items-center p-4 rounded-lg shadow-xs dark:bg-blue-700 bg-blue-500 col-span-2"
+                    class="flex items-center p-4 rounded-lg shadow-xs bg-blue-500 col-span-2"
                 >
                     <div>
-                        <p
-                            class="mb-2 text-sm font-medium text-white dark:text-white"
-                        >
+                        <p class="mb-2 text-sm font-medium text-white">
                             SISA ANGGARAN PELATIHAN
                         </p>
 
-                        <p
-                            class="text-xl font-semibold text-blue-900 dark:text-white"
-                        >
+                        <p class="text-xl font-semibold text-blue-900">
                             Rp. 000,000,000
                         </p>
                     </div>
@@ -172,7 +142,7 @@
                     <table class="w-full whitespace-no-wrap">
                         <thead>
                             <tr
-                                class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+                                class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b bg-gray-50"
                             >
                                 <th class="px-4 py-3 text-center">
                                     #
@@ -218,13 +188,11 @@
                                 </th>
                             </tr>
                         </thead>
-                        <tbody
-                            class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
-                        >
+                        <tbody class="bg-white divide-y">
                             <tr
                                 v-for="pel in pelatihan"
                                 :key="pel.no"
-                                class="text-gray-700 dark:text-gray-400"
+                                class="text-gray-700"
                             >
                                 <td :class="dataclass">
                                     <span
@@ -290,7 +258,7 @@
                     </table>
                 </div>
                 <div
-                    class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
+                    class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t bg-gray-50 sm:grid-cols-9"
                 >
                     <span class="flex items-center col-span-3">
                         Showing 1-10 of 100
